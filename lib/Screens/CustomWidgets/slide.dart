faf2aa86bbcb946ff28e72d3e659fc68ccec753f
Nicolas Barbosa <nicolasbarbosa1400@gmail.com>
2024-02-22 10:18:37 -0300

    Add carrousel

--- lib/Screens/CustomWidgets/slide.dart
+++ lib/Screens/CustomWidgets/slide.dart
@@ -10,119 +10,124 @@ class Slide extends StatefulWidget {
 }
 
 class _SlideState extends State<Slide> {
-  
   PageController pageController = PageController();
   List<double> progresso = [0.0, 0.0, 0.0];
   int qtdPaginas = 3;
-  int paginaAtual =  0;
+  int paginaAtual = 0;
 
-//metodo que é chamado antes da pagina ser construída para o efeito 
+//metodo que é chamado antes da pagina ser construída para o efeito
 //acontecer logo de carregada
-@override
-void initState(){
-  super.initState();
-  nextPage();
-}
+  @override
+  void initState() {
+    super.initState();
+    nextPage();
+  }
 
 //metodo para mudar de pagina periodicamente
-  nextPage(){
+  nextPage() {
     Timer.periodic(const Duration(seconds: 3), (timer) {
-      int page = pageController.page!.round();
-      setState(() {
-        paginaAtual = page;
-      });
+      //variavel para comparar com a quantidade de paginas
+      int proximaPagina = paginaAtual + 1;
 
-      if(page >= 3){
-        pageController.animateToPage(0, duration: const Duration(seconds: 2), curve: Curves.easeInCubic);
-      } else{
-        pageController.nextPage(duration: const Duration(seconds: 2), curve: Curves.ease);
+      if (proximaPagina >= qtdPaginas) {
+        //qnd a variavel for = a quantidade de paginas, reseta as paginas
+        proximaPagina = 0;
       }
-     
-     });
+
+      pageController
+          .animateToPage(proximaPagina,
+              duration: Duration(milliseconds: 300), curve: Curves.linear)
+          .then((_) {
+        setState(() {
+          paginaAtual = proximaPagina;
+          reset();
+        });
+      });
+    });
   }
 
 //iniciar o progresso
-  void iniciarProgresso(){
-      Timer.periodic(const Duration(milliseconds: 50), (timer) {
-          setState(() {
-            if(progresso[paginaAtual] <1){
-              progresso[paginaAtual] += 0.02;
-            }else{
-              timer.cancel();
-            }
-          });
-       });
+  void iniciarProgresso() {
+    Timer.periodic(const Duration(milliseconds: 50), (timer) {
+      setState(() {
+        if (progresso[paginaAtual] < 1) {
+          progresso[paginaAtual] += 0.02;
+        } else {
+          timer.cancel();
+        }
+      });
+    });
   }
 
 //metodo para resetar a animação
-void reset(){
-  for(int i=0; i < qtdPaginas; i++){
-    progresso[i]=0.0;
+  void reset() {
+    for (int i = 0; i < qtdPaginas; i++) {
+      progresso[i] = 0.0;
+    }
+    iniciarProgresso();
   }
-  iniciarProgresso();
-}
 
 //metodo para criar um indicator
-List<Widget> buildIndicator(){
-  List<Widget> lista = [];
-  for(int i=0; i < qtdPaginas; i++ ){
-   lista.add(Container(
-    width: 50,
-    height: 5,
-    margin: const EdgeInsets.all(8),
-    child: LinearProgressIndicator(
-      borderRadius: BorderRadius.circular(8),
-      value: progresso[i],
-      backgroundColor: Colors.grey[200],
-      valueColor: AlwaysStoppedAnimation<Color>(paginaAtual == i? Colors.blue : Colors.grey),
-    ),
-   ));
-    
-    }     
-    
-    return lista;             
-}
+  List<Widget> buildIndicator() {
+    List<Widget> lista = [];
+    for (int i = 0; i < qtdPaginas; i++) {
+      lista.add(Container(
+        width: 50,
+        height: 5,
+        margin: const EdgeInsets.all(8),
+        child: LinearProgressIndicator(
+          borderRadius: BorderRadius.circular(8),
+          value: progresso[i],
+          backgroundColor: Colors.grey[200],
+          valueColor: AlwaysStoppedAnimation<Color>(
+              paginaAtual == i ? Colors.blue : Colors.grey),
+        ),
+      ));
+    }
+
+    return lista;
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     return Container(
-      margin: const EdgeInsets.all(16),
-      width: double.infinity,
-      height: 150,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8)
-      ),
-      child: Stack(
-        alignment: AlignmentDirectional.bottomCenter,
-        children: [
-           PageView(
-        controller: pageController,
-        children:[
-          Container(
-            width: double.infinity,
-            height: 200,
-            color: Colors.orange,
-          ),
-          Container(
-            width: double.infinity,
-            height: 200,
-            color: Colors.black,
-          ),
-          Container(
-            width: double.infinity,
-            height: 200,
-            color: Colors.pink,
-          ),
-          Container(
-            width: double.infinity,
-            height: 200,
-            color: Colors.cyan,
-          ),
-
-        ]
-      ),
-        ],)
-    );
+        margin: const EdgeInsets.all(16),
+        width: double.infinity,
+        height: 150,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+        child: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            PageView(controller: pageController, children: [
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.orange,
+              ),
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.black,
+              ),
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.pink,
+              ),
+              Container(
+                width: double.infinity,
+                height: 200,
+                color: Colors.cyan,
+              ),
+            ]),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: buildIndicator(),
+              ),
+            )
+          ],
+        ));
   }
 }
